05de871a90657df3ec95295e859efbe8203678a8
@mastytch <andriy.mostovyy@gmail.com>
2015-02-16 15:15:51 +0200

    2

--- src/randchar.java
+++ src/randchar.java
@@ -1,5 +1,39 @@
-/**
- * Created by Андрей on 16.02.2015.
- */
+import java.util.Scanner;
+
+
+
 public class randchar {
+
+    public static void main(String[] args) {
+
+        int xnumber;
+
+        xnumber = (int) (Math.random() * 99 + 1);
+
+        Scanner keyboard = new Scanner(System.in);
+
+        int answer;
+
+        do {
+
+            System.out.print("Enter a guess (1-100): ");
+
+            answer = keyboard.nextInt();
+
+            if (answer == xnumber)
+
+                System.out.println("Your guess is correct. Congratulations!");
+
+            else if (answer < xnumber)
+
+                System.out.println("Your guess is smaller than the secret number.");
+
+            else if (answer > xnumber)
+
+                System.out.println("Your guess is greater than the secret number.");
+
+        } while (answer != xnumber);
+
+    }
+
 }
